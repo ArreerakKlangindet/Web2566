110153b72fc00c4fd504c3a0d48e9bfb6668bfa5
ArreerakKlangindet <137706452+ArreerakKlangindet@users.noreply.github.com>
2024-03-16 22:40:29 +0700

    Add files via upload

--- work6.js
+++ work6.js
@@ -1,6 +1,40 @@
 const RB=ReactBootstrap;
 const {Alert, Card, Button, Table} = ReactBootstrap;
 
+function StudentTable({data, app}){
+  var rows = [];
+  for(var s of data){      
+     rows.push(<tr>
+      <td>{s.id}</td>
+      <td>{s.title}</td>
+      <td>{s.fname}</td>
+      <td>{s.lname}</td>
+      <td>{s.email}</td>
+      </tr>);
+  }
+  return <table className='table'>
+  <tr>
+      <td>รหัส</td>
+      <td>คำนำหน้า</td>
+      <td>ชื่อ</td>
+      <td>สกุล</td>
+      <td>email</td>
+      </tr>
+      { rows }
+  </table>
+}
+
+function TextInput({label,app,value,style}){
+  return <label className="form-label">
+  {label}:    
+   <input className="form-control" style={style}
+   value={app.state[value]} onChange={(ev)=>{
+       var s={};
+       s[value]=ev.target.value;
+       app.setState(s)}
+   }></input>
+ </label>;  
+}
 
 class App extends React.Component {
     title = (
@@ -16,33 +50,87 @@ class App extends React.Component {
     );
     state = {
         scene: 0,
+        students:[],
+        stdid:"",
+        stdtitle:"",
+        stdfname:"",
+        stdlname:"",
+        stdemail:"",
+        stdphone:"",
     }      
     render() {
+      // var stext = JSON.stringify(this.state.students);  
       return (
         <Card>
           <Card.Header>{this.title}</Card.Header>  
-          <Card.Body></Card.Body>
+          <Card.Body>
+            <Button onClick={()=>this.readData()}>Read Data</Button>
+            <Button onClick={()=>this.autoRead()}>Auto Read</Button>
+            <div>
+            <StudentTable data={this.state.students} app={this}/>  
+            </div>
+          </Card.Body>
+          <Card.Footer>
+          <b>เพิ่ม/แก้ไขข้อมูล นักศึกษา :</b><br/>
+          <TextInput label="ID" app={this} value="stdid" style={{width:120}}/>  
+          <TextInput label="คำนำหน้า" app={this} value="stdtitle" style={{width:100}} />
+          <TextInput label="ชื่อ" app={this} value="stdfname" style={{width:120}}/>
+          <TextInput label="สกุล" app={this} value="stdlname" style={{width:120}}/>
+          <TextInput label="Email" app={this} value="stdemail" style={{width:150}} />        
+          <TextInput label="Phone" app={this} value="stdphone" style={{width:120}}/>
+          <Button onClick={()=>this.insertData()}>Save</Button>
+          </Card.Footer>
           <Card.Footer>{this.footer}</Card.Footer>
         </Card>          
       );
-    }      
+    }
+    readData(){
+        db.collection("students").get().then((querySnapshot) => {
+            var stdlist=[];
+            querySnapshot.forEach((doc) => {
+                stdlist.push({id:doc.id,... doc.data()});                
+            });
+            console.log(stdlist);
+            this.setState({students: stdlist});
+        });
+    }
+    autoRead(){
+        db.collection("students").onSnapshot((querySnapshot) => {
+            var stdlist=[];
+            querySnapshot.forEach((doc) => {
+                stdlist.push({id:doc.id,... doc.data()});                
+            });          
+            this.setState({students: stdlist});
+        });
+    }
+    insertData(){
+      db.collection("students").doc(this.state.stdid).set({
+         title : this.state.stdtitle,
+         fname : this.state.stdfname,
+         lname : this.state.stdlname,
+         phone : this.state.stdphone,
+         email : this.state.stdemail,
+      });
   }
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAA5uXrhE-FU4N3cfxUJZzmMA43UEjvlZA",
-    authDomain: "web2566-2bf26.firebaseapp.com",
-    projectId: "web2566-2bf26",
-    storageBucket: "web2566-2bf26.appspot.com",
-    messagingSenderId: "370703544481",
-    appId: "1:370703544481:web:a6fcc90c5df1fae85a80e0"
+
+  }
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAA5uXrhE-FU4N3cfxUJZzmMA43UEjvlZA",
+  authDomain: "web2566-2bf26.firebaseapp.com",
+  projectId: "web2566-2bf26",
+  storageBucket: "web2566-2bf26.appspot.com",
+  messagingSenderId: "370703544481",
+  appId: "1:370703544481:web:a6fcc90c5df1fae85a80e0"
   };
 firebase.initializeApp(firebaseConfig);      
 const db = firebase.firestore();
-db.collection("students").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} =>`,doc.data());
-  });
-});
+//db.collection("students").get().then((querySnapshot) => {
+//  querySnapshot.forEach((doc) => {
+//      console.log(`${doc.id} =>`,doc.data());
+//  });
+// });
 
 
   const container = document.getElementById("myapp");
