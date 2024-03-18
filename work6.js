6fa36b8137af5c587535a4e61d8ae5134323eb4a
ArreerakKlangindet <137706452+ArreerakKlangindet@users.noreply.github.com>
2024-03-18 11:39:38 +0700

    Add files via upload

--- work6.js
+++ work6.js
@@ -1,138 +1,220 @@
-const RB=ReactBootstrap;
-const {Alert, Card, Button, Table} = ReactBootstrap;
+const RB = ReactBootstrap;
+const { Alert, Card, Button, table } = ReactBootstrap
 
-function StudentTable({data, app}){
-  var rows = [];
-  for(var s of data){      
-     rows.push(<tr>
-      <td>{s.id}</td>
-      <td>{s.title}</td>
-      <td>{s.fname}</td>
-      <td>{s.lname}</td>
-      <td>{s.email}</td>
-      </tr>);
-  }
-  return <table className='table'>
-  <tr>
-      <td>รหัส</td>
-      <td>คำนำหน้า</td>
-      <td>ชื่อ</td>
-      <td>สกุล</td>
-      <td>email</td>
-      </tr>
-      { rows }
-  </table>
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+
+function EditButton({ std, app }) {
+    return <button onClick={() => app.edit(std)}>แก้ไข</button>
 }
 
-function TextInput({label,app,value,style}){
-  return <label className="form-label">
-  {label}:    
-   <input className="form-control" style={style}
-   value={app.state[value]} onChange={(ev)=>{
-       var s={};
-       s[value]=ev.target.value;
-       app.setState(s)}
-   }></input>
- </label>;  
+
+function DeleteButton({ std, app }) {
+    return <button onClick={() => app.delete(std)}>ลบ</button>
 }
+
+
+//Use for loop
+// function StudentTable({ data, app }) {
+//     var rows = [];
+//     for (var s of data) {
+//         rows.push(<tr>
+//             <td>{s.id}</td>
+//             <td>{s.title}</td>
+//             <td>{s.fname}</td>
+//             <td>{s.lname}</td>
+//             <td>{s.email}</td>
+//         </tr>);
+//     }
+//     return <table className='table'>
+//         <tr>
+//             <td>รหัส</td>
+//             <td>คำนำหน้า</td>
+//             <td>ชื่อ</td>
+//             <td>สกุล</td>
+//             <td>email</td>
+//         </tr>
+//         {rows}
+//     </table>
+// }
+
+
+// use Array.map
+function StudentTable({ data, app }) {
+    return <table className='table mt-5'>
+        <thead>
+            <tr>
+                <th>รหัส</th>
+                <th>คำนำหน้า</th>
+                <th>ชื่อ</th>
+                <th>สกุล</th>
+                <th>email</th>
+                <th>Phone</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                data.map((s) => <tr className="table-info">
+                    <td>{s.id}</td>
+                    <td>{s.title}</td>
+                    <td>{s.fname}</td>
+                    <td>{s.lname}</td>
+                    <td>{s.email}</td>
+                    <td>{s.phone}</td>
+                    <td><EditButton std={s} app={app} /></td>
+                    <td><DeleteButton std={s} app={app} /></td>
+                </tr>)
+            }
+        </tbody>
+    </table>
+}
+
+
+// TextInput
+function TextInput({ label, app, value, style }) {
+    return <label className="form-label">
+        {label}:
+        <input className="form-control" style={style}
+            value={app.state[value]} onChange={(ev) => {
+                var s = {};
+                s[value] = ev.target.value;
+                app.setState(s)
+            }
+            }></input>
+    </label>;
+}
+
 
 class App extends React.Component {
+
+    // config header 
     title = (
-      <Alert variant="info">
-        <b>Work6 :</b> Firebase
-      </Alert>
+        <RB.Alert variant="info">
+            <b>Work6 :</b> Firebase
+        </RB.Alert>
     );
+
+    // config footer
     footer = (
-      <div>
-        By 643021345-9 Arreerak Klangindet <br />
-        College of Computing, Khon Kaen University
-      </div>
+        <div>
+            By 643021345-9 arreerak klangindet <br />
+            College of Computing, Khon Kaen University
+        </div>
     );
-    state = {
-        scene: 0,
-        students:[],
-        stdid:"",
-        stdtitle:"",
-        stdfname:"",
-        stdlname:"",
-        stdemail:"",
-        stdphone:"",
-    }      
-    render() {
-      // var stext = JSON.stringify(this.state.students);  
-      return (
-        <Card>
-          <Card.Header>{this.title}</Card.Header>  
-          <Card.Body>
-            <Button onClick={()=>this.readData()}>Read Data</Button>
-            <Button onClick={()=>this.autoRead()}>Auto Read</Button>
-            <div>
-            <StudentTable data={this.state.students} app={this}/>  
-            </div>
-          </Card.Body>
-          <Card.Footer>
-          <b>เพิ่ม/แก้ไขข้อมูล นักศึกษา :</b><br/>
-          <TextInput label="ID" app={this} value="stdid" style={{width:120}}/>  
-          <TextInput label="คำนำหน้า" app={this} value="stdtitle" style={{width:100}} />
-          <TextInput label="ชื่อ" app={this} value="stdfname" style={{width:120}}/>
-          <TextInput label="สกุล" app={this} value="stdlname" style={{width:120}}/>
-          <TextInput label="Email" app={this} value="stdemail" style={{width:150}} />        
-          <TextInput label="Phone" app={this} value="stdphone" style={{width:120}}/>
-          <Button onClick={()=>this.insertData()}>Save</Button>
-          </Card.Footer>
-          <Card.Footer>{this.footer}</Card.Footer>
-        </Card>          
-      );
-    }
-    readData(){
+
+
+    readData() {
         db.collection("students").get().then((querySnapshot) => {
-            var stdlist=[];
+            var stdlist = [];
             querySnapshot.forEach((doc) => {
-                stdlist.push({id:doc.id,... doc.data()});                
+                stdlist.push({ id: doc.id, ...doc.data() });
             });
             console.log(stdlist);
-            this.setState({students: stdlist});
+            this.setState({ students: stdlist });
         });
     }
-    autoRead(){
+
+
+    autoRead() {
         db.collection("students").onSnapshot((querySnapshot) => {
-            var stdlist=[];
+            var stdlist = [];
             querySnapshot.forEach((doc) => {
-                stdlist.push({id:doc.id,... doc.data()});                
-            });          
-            this.setState({students: stdlist});
+                stdlist.push({ id: doc.id, ...doc.data() });
+            });
+            this.setState({ students: stdlist });
         });
     }
-    insertData(){
-      db.collection("students").doc(this.state.stdid).set({
-         title : this.state.stdtitle,
-         fname : this.state.stdfname,
-         lname : this.state.stdlname,
-         phone : this.state.stdphone,
-         email : this.state.stdemail,
-      });
-  }
 
 
-  }
+    state = {
+        scene: 0,
+        students: [],
+        stdid: "",
+        stdtitle: "",
+        stdfname: "",
+        stdlname: "",
+        stdemail: "",
+        stdphone: "",
+    }
+
+
+    insertData() {
+        db.collection("students").doc(this.state.stdid).set({
+            title: this.state.stdtitle,
+            fname: this.state.stdfname,
+            lname: this.state.stdlname,
+            phone: this.state.stdphone,
+            email: this.state.stdemail,
+        });
+    }
+
+
+    edit(std) {
+        this.setState({
+            stdid: std.id,
+            stdtitle: std.title,
+            stdfname: std.fname,
+            stdlname: std.lname,
+            stdemail: std.email,
+            stdphone: std.phone,
+        })
+    }
+
+
+    delete(std) {
+        if (confirm("ต้องการลบข้อมูล")) {
+            db.collection("students").doc(std.id).delete();
+        }
+    }
+
+    render() {
+        // var stext = JSON.stringify(this.state.students);  
+        return (
+            <Card>
+                <Card.Header>{this.title}</Card.Header>
+                <Card.Body className="content">
+                    <Button onClick={() => this.readData()} style={{marginRight : 10}}>Read Data</Button>
+                    <Button onClick={() => this.autoRead()}>Auto Read</Button>
+                    <div className="table">
+                        <StudentTable data={this.state.students} app={this} />
+                    </div>
+                </Card.Body>
+                <Card.Footer>
+                    <b>เพิ่ม/แก้ไขข้อมูล นักศึกษา :</b><br />
+                    <TextInput placeholder="Hello" label="ID" app={this} value="stdid" style={{ width: 120 }} />
+                    <TextInput label="คำนำหน้า" app={this} value="stdtitle" style={{ width: 100, marginLeft: 10 }} />
+                    <TextInput label="ชื่อ" app={this} value="stdfname" style={{ width: 120, marginLeft: 10 }} />
+                    <TextInput label="สกุล" app={this} value="stdlname" style={{ width: 120, marginLeft: 10 }} />
+                    <TextInput label="Email" app={this} value="stdemail" style={{ width: 150, marginLeft: 10 }} />
+                    <TextInput label="Phone" app={this} value="stdphone" style={{ width: 120, marginLeft: 10 }} />
+                    <Button onClick={() => this.insertData()} style={{ marginLeft: 10 }}>Save</Button>
+                </Card.Footer>
+                <Card.Footer>{this.footer}</Card.Footer>
+            </Card>
+        );
+    }
+}
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAA5uXrhE-FU4N3cfxUJZzmMA43UEjvlZA",
-  authDomain: "web2566-2bf26.firebaseapp.com",
-  projectId: "web2566-2bf26",
-  storageBucket: "web2566-2bf26.appspot.com",
-  messagingSenderId: "370703544481",
-  appId: "1:370703544481:web:a6fcc90c5df1fae85a80e0"
-  };
-firebase.initializeApp(firebaseConfig);      
+  apiKey: "AIzaSyDZMemDrhI9jGRzufUzLn2_8vG6koBudqA",
+  authDomain: "web2566-5914d.firebaseapp.com",
+  projectId: "web2566-5914d",
+  storageBucket: "web2566-5914d.appspot.com",
+  messagingSenderId: "590626703794",
+  appId: "1:590626703794:web:6e6c05079a5920553e3cab",
+  measurementId: "G-8J965MV4G3"
+};
+
+firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-//db.collection("students").get().then((querySnapshot) => {
-//  querySnapshot.forEach((doc) => {
-//      console.log(`${doc.id} =>`,doc.data());
-//  });
+// db.collection("students").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} =>`, doc.data());
+//     });
 // });
 
 
-  const container = document.getElementById("myapp");
-  const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+
+const container = document.getElementById("myapp");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
